4818e240ed6cd2e34a4cbb0087cab4cc261b4276
Angel Szymczak <angel.szymczak@hotmail.com>
2023-11-11 13:33:42 -0300

    feat: Setup third-party Devise for User-Authentication
    - bundler add devise
    - rails g devise:install
    - rails g devise:views
    - rails g devise User

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {
+  # host: "should-be-set-to-the-actual-host",
+  # port: 3_000,
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+_eve, _nahir, _laura = User.create(
+  [
+    {
+      email: "eve@mail.com",
+      password: 123_456,
+      password_confirmation: 123_456
+    },
+    {
+      email: "nahir@mail.com",
+      password: 123_456,
+      password_confirmation: 123_456
+    },
+    {
+      email: "laura@mail.com",
+      password: 123_456,
+      password_confirmation: 123_456
+    },
+  ]
+)
+
